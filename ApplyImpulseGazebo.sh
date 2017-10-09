594f6ce89acab2bf90385e77078420c999939f01
Gabriele <gabriele.nava@mail.polimi.it>
2017-10-09 23:52:55 +0200

    working gazeb impulse plugin

--- ApplyImpulseGazebo.sh
+++ ApplyImpulseGazebo.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# Synchronization with Gazebo for applying input always at the same time
-yarpdatadumper 
-yarp connect /Simulink/time /dump
+# Synchronization with Gazebo for applying input always at the same time. This script is called from Matlab while initializing Simulink model, and it is assumed that after 20s an impulsive force is applied to the robot. It is also assumed the usage of a fixed step integrator, with t_step = 0.01s.
+
+n=1
+
+# continue until $n equals 20*100 = 2000
+while [ $n -le 2000 ]
+do
+	sleep 0.01
+	n=$(( n+1 )) # increment $n
+done
 
 echo "Applying impulse force on robot torso..."
 
-echo "0.0 100.0 0.0"  | yarp write ... /Gazebo/ApplyTorsoForce:i
+echo "0.0 150.0 0.0"  | yarp write ... /Gazebo/ApplyTorsoForce:i
 
 sleep 0.01
 
@@ -14,7 +21,5 @@ echo "0.0 0.0 0.0"  | yarp write ... /Gazebo/ApplyTorsoForce:i
 
 echo "Done!"
 
-rm -r /dump
-echo "Removed dumped time"
 
 
