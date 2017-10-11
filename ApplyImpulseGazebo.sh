482f03f88d2c8369c1bf3d3d6e2a5c39d54f493e
Gabriele <gabriele.nava@mail.polimi.it>
2017-10-11 10:50:32 +0200

    final version for impusive force plugin

--- ApplyImpulseGazebo.sh
+++ ApplyImpulseGazebo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Synchronization with Gazebo for applying input always at the same time. This script is called from Matlab while initializing Simulink model, and it is assumed that after 20s an impulsive force is applied to the robot. It is also assumed the usage of a fixed step integrator, with t_step = 0.01s.
+# Synchronization with Gazebo for applying input always at the same time. This script is called from Matlab while initializing Simulink model, and it is assumed that after 30s an impulsive force is applied to the robot. It is also assumed the usage of a fixed step integrator, with t_step = 0.01s.
 
 n=1
 
-# continue until $n equals 20*100 = 2000
-while [ $n -le 2000 ]
+# continue until $n equals 30*100 = 3000
+while [ $n -le 3000 ]
 do
 	sleep 0.01
 	n=$(( n+1 )) # increment $n
@@ -13,7 +13,7 @@ done
 
 echo "Applying impulse force on robot torso..."
 
-echo "0.0 150.0 0.0"  | yarp write ... /Gazebo/ApplyTorsoForce:i
+echo "0.0 100.0 0.0"  | yarp write ... /Gazebo/ApplyTorsoForce:i
 
 sleep 0.01
 
